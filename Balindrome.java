903742ce1fb60ca0677215db870fe114edd21501
SatheeshPa <43429020+SatheeshPa@users.noreply.github.com>
2019-11-20 15:37:13 -0500

    Balindrome
    Balindrome

---
+++ Balindrome.java
@@ -0,0 +1,28 @@
+package com.test;
+
+public class Balindrome {
+	public static boolean checkPalindrome(String str) {		
+		int i = 0;
+		int j = str.length()-1;
+		boolean check = true;
+		
+		while(i<j) {
+			if(str.charAt(i) != str.charAt(j)) {
+				check = false;
+			}
+			i++;
+			j--;
+		}
+		return check;
+	}
+
+	public static void main(String[] args) {
+		
+		String str = "testtest";
+		System.out.println("Test");
+		System.out.println(checkPalindrome(str)?"This is polindrome": "This is not a polindrome");
+		
+
+	}
+
+}
